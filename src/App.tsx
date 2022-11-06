7c070a5aed65a9928dbfba2d752177e36b8b0c5d
vladimir817vk@gmail.com <vladimir817vk@gmail.com>
2022-11-06 03:45:08 +0300

    add localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CounterClicks, {CounterType} from "./CounterClisks";
@@ -6,10 +6,22 @@ import {Input} from "./Input";
 import {Button} from "./Button";
 import CounterClisks from "./CounterClisks";
 
+
 function App(props: any) {
     //
     const [inputMaxValue, setInputMaxValue] = useState<any>(0)
     const [inputStartValue, setInputStartValue] = useState<any>(0)
+
+    //get from localStorage on update
+    useEffect(() => {
+        getFromLocalStorage()
+    }, [])
+
+    //set localStorage on change inputStartValue and inputMaxValue
+    useEffect(() => {
+        onClickSet()
+    }, [inputStartValue, inputMaxValue])
+
     const onChangeSetMaxInput = (e: ChangeEvent<HTMLInputElement>) => {
         setInputMaxValue(Number(e.currentTarget.value))
     }
@@ -24,8 +36,37 @@ function App(props: any) {
                 setClicks(++clicks)
             }
         }
+
         const onClickSet = () => {
             setClicks(inputStartValue)
+            localStorage.setItem('inputStartValue', JSON.stringify(inputStartValue)) //set to localStorage
+            localStorage.setItem('inputMaxValue', JSON.stringify(inputMaxValue))     //set to localStorage
+        }
+
+        // get localStorage
+        const getFromLocalStorage = () => {
+        let valueToNumberStart = localStorage.getItem('inputStartValue');       //get from localStorage
+        let valueToNumberMax = localStorage.getItem('inputMaxValue');           //get from localStorage
+        if (valueToNumberStart) {
+            let numberStart = JSON.parse(valueToNumberStart)
+            setInputStartValue(numberStart)
+        }
+        if (valueToNumberMax){
+            let numberMax = JSON.parse(valueToNumberMax)
+            setInputMaxValue(numberMax)
+        }
+        }
+
+        // clear localStorage
+        const clearLocalStorage = () => {
+        localStorage.clear();
+        setInputStartValue(0);
+        setInputMaxValue(0)
+        }
+
+        // delete Item from localStorage
+        const removeLocalStorage = () => {
+        localStorage.removeItem('inputStartValue')
         }
 
         const resetButton = () => {
@@ -51,6 +92,12 @@ function App(props: any) {
         <Button title={'inc'} onClick={clickCounter} disabled={disabledInc}/>
         <Button title={'reset'} onClick={resetButton} disabled={disabledReset}/>
         <Button title={'set'} onClick={onClickSet} disabled={disabledSet}/>
+        <div className={'localStorage'}>
+            LocalStorage
+            <Button title={'get from localStorage'} onClick={getFromLocalStorage} disabled={disabledSet}/>
+            <Button title={'clear localStorage'} onClick={clearLocalStorage} disabled={disabledSet}/>
+            <Button title={'remove startValue from localStorage'} onClick={removeLocalStorage} disabled={disabledSet}/>
+        </div>
         {/*<Button title={'set'} onClickInc={clickCounter} disabledInc={clicks < 5 ? false : true} onClickReset={resetButton} disabledReset={clicks < 1 ? true : false}/>*/}
         {/*<div className='name1'>*/}
         {/*    <div className='blocks'>*/}
